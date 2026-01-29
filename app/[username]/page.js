0ad92c0a270633985211252b19f5455889a3774d
Arun Shivhare <arunshivhare925@gmail.com>
2026-01-30 00:04:16 +0530

    making user profile

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -6,9 +6,16 @@ const Username = async ({params}) => {
 
   return (
     <>
-    {username}
-    <div className=''>
+    <div className='cover relative'>
       <img className='object-cover w-full' src="cover.png" alt="" />
+      <div className=' -bottom-18 absolute right-[46%] border-2 border-black rounded-full'>
+        <img className='rounded-full' width={123} height={123} src="profile.png" alt="" />
+      </div>
+    </div>
+    <div className="info flex justify-center items-center my-19">
+      @{username}
+      <div></div>
+      <div></div>
     </div>
     </>
   )
