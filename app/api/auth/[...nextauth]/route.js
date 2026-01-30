28ae744186c27fa9cdbd5512f9d77dc2858fcdac
Arun Shivhare <arunshivhare925@gmail.com>
2026-01-30 23:17:31 +0530

    connecting database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,6 +4,10 @@ import NextAuth from 'next-auth'
 // import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from 'next-auth/providers/github'
+import mongoose from 'mongoose'
+import User from '@/models/user'
+import Payment from '@/models/payment'
+import connectDB from '@/db/connectDB'
 
 export const authoptions = NextAuth({
     providers: [
@@ -30,7 +34,33 @@ export const authoptions = NextAuth({
         //     server: process.env.MAIL_SERVER,
         //     from: 'NextAuth.js <no-reply@example.com>'
         // }),
-    ]
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account.provider == "github") {
+                await connectDB()
+                //check if user exist in database
+                const cuurentuser = await User.findOne({ email: email })
+                if (!cuurentuser) {
+                    //create a new user
+                    const newuser = await User.create({
+                        email: user.email,
+                        username: user.email.split("@")[0],
+                    })
+                    user.name = newuser.username
+                }
+                else {
+                    user.name = cuurentuser.username
+                }
+                return true
+            }
+        },
+        async session({ session, user, token }) {
+            const dbUser = await User.find({ email: session.user.email })
+            session.user.name = dbUser.username
+            return session
+        },
+    }
 })
 
 export { authoptions as GET, authoptions as POST }
---
+++ db/connectDB.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in .env.local")
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose)
+  }
+
+  cached.conn = await cached.promise
+  console.log("MongoDB connected")
+  return cached.conn
+}
+
+export default connectDB
